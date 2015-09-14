100821176ed54a7b067a2edf979ddcd1c17bd2d9
ZoeAde <zoe.a.adelman@gmail.com>
2015-09-14 13:33:15 -0600

    removing unnecessary backend files

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,13 +1,96 @@
+//////////////////////////// ANGULAR /////////////////////
+var app = angular.module("myApp", ['ngRoute']);
+
+//ANGULAR ROUTES
+app.config(function($routeProvider) {
+  $routeProvider
+        // .when('/', {
+        //     templateUrl: 'pages/welcome.html',
+        //     controller: 'welcomeCtrl',
+        //     title: 'Gender Gap Defined'
+        // })
+        .when('/', {
+            templateUrl: 'pages/comment.html',
+            controller: 'mainController',
+            // title: 'Gender Gap Defined'
+        })
+        .when('/bar', {
+            templateUrl: 'pages/bar-graph.html',
+            controller: 'mainController',
+            // title: 'Gender Gap Bar Graph'
+        })
+        .when('/table', {
+            templateUrl: 'pages/table.html',
+            controller: 'mainController',
+            // title: 'Gender Gap Table'
+        })
+        .when('/company-stats/:id', {
+            templateUrl: 'pages/company-stats.html',
+            controller: 'mainController',
+            // title: 'Gender Gap Company Profile'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+//ANGULAR CONTROLLERS
+app.controller('mainController', ['$scope', '$location', '$http', function($scope, $location, $http) {
+
+  $scope.go = function(hash) {
+    $location.path(hash);
+  };
+
+
+  $http.get('https://spreadsheets.google.com/feeds/list/0AlZH8QBl60oodEJTdFA5TlZOcDJCMU02RkZoSHF5SHc/od6/public/values?alt=json').success(function(response) {
+
+    var object = response.feed.entry;
+    console.log(parsedObject);
+    var parsedObject = createStatisticArray(object);
+    $scope.statistics = parsedObject;
+    $scope.sortType = "company";
+    $scope.sortReverse = false;
+    $scope.searchStats = "";
+
+    $scope.message = "hello";
+  });
+}]);
+
+
+//DEFINING ROUTES//
+// myApp.run(['$location', '$rootScope', function($location, $rootScope) {
+//     $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+//         // test for current route
+//         if(current.$$route) {
+//             // Set current page title
+//             $rootScope.title = current.$$route.title;
+//         }
+//     });
+// }])
+
+//END DEFINING ROUTES//
+
+
+//ANGULAR FOR TABLE GRAPH//
 function createStatisticArray(object) {
   var companyArray = [];
+  var totalStats = [];
   for (var i = 0; i < object.length; i++) {
-    var company = object[i].gsx$company.$t;
-    var female_eng = object[i].gsx$numfemaleeng.$t;
-    var num_eng = object[i].gsx$numeng.$t;
-    var percent_female = object[i].gsx$percentfemaleeng.$t;
-    var stat = new Statistic(company, female_eng, num_eng, percent_female);
-    companyArray.push(stat);
-  }
+    // if (i === 0) {
+    //   var companies = object[0].gsx$company.$t;
+    //   var total_female_eng = object[0].gsx$numfemaleeng.$t;
+    //   var total_num_eng = object[0].gsx$numeng.$t;
+    //   var total_percent_female = object[0].gsx$percentfemaleeng.$t;
+    //   var allStats = new Statistic(companies, Number(total_female_eng), Number(total_num_eng), Number(total_percent_female));
+    //   totalStats.push(allStats);
+    // } else {
+      var company = object[i].gsx$company.$t;
+      var female_eng = object[i].gsx$numfemaleeng.$t;
+      var num_eng = object[i].gsx$numeng.$t;
+      var percent_female = object[i].gsx$percentfemaleeng.$t;
+      var stat = new Statistic(company, Number(female_eng), Number(num_eng), Number(percent_female));
+      companyArray.push(stat);
+      }
   return companyArray;
 }
 
@@ -17,52 +100,4 @@ function Statistic(company, female_eng, num_eng, percent_female) {
   this.num_eng = num_eng;
   this.percent_female = percent_female;
 }
-
-////////// ANGULAR VERSION////////
-var app = angular.module("myApp", []);
-app.controller('companyController', function($scope, $http) {
-
-    $http.get('https://spreadsheets.google.com/feeds/list/0AlZH8QBl60oodEJTdFA5TlZOcDJCMU02RkZoSHF5SHc/od6/public/values?alt=json').success(function(response) {
-
-      var object = response.feed.entry;
-      var parsedObject = createStatisticArray(object);
-      $scope.statistics = parsedObject;
-    });
-});
-
-/////////// jQUERY/AJAX VERSION ////////////
-// var JSONURL = 'https://spreadsheets.google.com/feeds/list/0AlZH8QBl60oodEJTdFA5TlZOcDJCMU02RkZoSHF5SHc/od6/public/values?alt=json';
-
-// function callback(data){
-//     var rows = [];
-//     var cells = data.feed.entry;
-//     // console.log(cells);
-//     // console.log(cells[1].gsx$numfemaleeng.$t);
-//     for (var i = 1; i < cells.length; i++){
-//         var company = cells[i].gsx$company.$t;
-//         var female_eng = cells[i].gsx$numfemaleeng.$t;
-//         var num_eng = cells[i].gsx$numeng.$t;
-//         var percent_female = cells[i].gsx$percentfemaleeng.$t;
-//         var rowObj = {company: company, femaleEngineers: female_eng, totalEngineers: num_eng, percentFemale: percent_female};
-//         // rows.push(rowObj);
-//         $('#tableBody').append("<tr><td>" + company + "</td><td>" + female_eng + "</td><td>" + num_eng + "</td><td>"+ percent_female + "</td></tr>");
-//     };
-// }
-
-// $(document).ready(function(){
-
-//     $.ajax({
-//         url:JSONURL,
-//         success: function(data){
-//             callback(data);
-//         }
-//     });
-
-// });
-
-///////////////////////////////////////
-
-
-
-
 
--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var swig = require('swig');
 
 
 // *** routes *** //
@@ -17,9 +16,9 @@ var app = express();
 
 
 // *** view engine *** //
-var swig = new swig.Swig();
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
+// var swig = new swig.Swig();
+// app.engine('html', swig.renderFile);
+// app.set('view engine', 'html');
 
 
 // *** static directory *** //
@@ -27,6 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 // *** config middleware *** //
+app.use(express.static(path.join(__dirname, '../client')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,6 +37,9 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 // *** main routes *** //
 app.use('/', routes);
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
